d8bd4afdb415762eb41fc086aa332e1a029a22e5
曹一梁 <caoyiliang@me.com>
2023-07-07 10:37:40 +0800

    添加项目文件。

---
+++ OnvifTest/Main.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace OnvifTest
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(800, 450);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.Location = new Point(649, 25);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Focus +";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += Button1_Click;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button2.Location = new Point(649, 54);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Focus -";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += Button2_Click;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button3.Location = new Point(649, 118);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Zoom +";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += Button3_Click;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button4.Location = new Point(649, 147);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 2;
+            button4.Text = "Zoom -";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += Button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ OnvifTest/Main.cs
@@ -0,0 +1,240 @@
+using System.ServiceModel.Channels;
+using System.ServiceModel;
+using Onvif.Security;
+using DeviceClient;
+using Imaging;
+using MediaService;
+using PtzService;
+using Emgu.CV;
+
+namespace OnvifTest
+{
+    public partial class Main : Form
+    {
+        ImagingPortClient? _imaging;
+        PTZClient? _ptz;
+        string? _token;
+        VideoCapture? _videoCapture;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var ip = "192.168.1.88";
+            var port = 8080;
+            var user = "admin";
+            var pass = "12345678h";
+            _imaging = await CreateImagingClientAsync($"{ip}:{port}", user, pass);
+            _ptz = await CreatePTZClientAsync($"{ip}:{port}", user, pass);
+            var media = await CreateMediaClientAsync($"{ip}:{port}", user, pass);
+            var profiles = await media.GetProfilesAsync();
+            _token = profiles.Profiles[0].token;
+            var mediaUri = (await media.GetStreamUriAsync(new StreamSetup(), _token)).Uri;
+            _videoCapture = new VideoCapture(mediaUri);
+            _videoCapture.ImageGrabbed += OnImageGrabbed;
+            _videoCapture.Start();
+        }
+
+        private void OnImageGrabbed(object? sender, EventArgs e)
+        {
+            var frame = new Mat();
+            try
+            {
+                if (_videoCapture!.Retrieve(frame) && !frame.IsEmpty)
+                {
+                    pictureBox1.Image = frame.ToBitmap();
+                }
+            }
+            finally
+            {
+                frame.Dispose();
+            }
+        }
+
+        static async Task<ImagingPortClient> CreateImagingClientAsync(string host, string username, string password)
+        {
+            var binding = CreateBinding();
+            var device = await CreateDeviceClientAsync(new Uri($"http://{host}/onvif/device_service"), username, password);
+            var caps = await device.GetCapabilitiesAsync(new CapabilityCategory[] { CapabilityCategory.Imaging });
+            var imaging = new ImagingPortClient(binding, new EndpointAddress(new Uri(caps.Capabilities.Imaging.XAddr)));
+
+            var time_shift = await GetDeviceTimeShift(device);
+            imaging.ChannelFactory.Endpoint.EndpointBehaviors.Clear();
+            imaging.ChannelFactory.Endpoint.EndpointBehaviors.Add(new SoapSecurityHeaderBehavior(username, password, time_shift));
+
+            // Connectivity Test
+            await imaging.OpenAsync();
+
+            return imaging;
+        }
+
+        static async Task<MediaClient> CreateMediaClientAsync(string host, string username, string password)
+        {
+            var binding = CreateBinding();
+            var device = await CreateDeviceClientAsync(new Uri($"http://{host}/onvif/device_service"), username, password);
+            var caps = await device.GetCapabilitiesAsync(new CapabilityCategory[] { CapabilityCategory.Media });
+            var media = new MediaClient(binding, new EndpointAddress(new Uri(caps.Capabilities.Media.XAddr)));
+
+            var time_shift = await GetDeviceTimeShift(device);
+            media.ChannelFactory.Endpoint.EndpointBehaviors.Clear();
+            media.ChannelFactory.Endpoint.EndpointBehaviors.Add(new SoapSecurityHeaderBehavior(username, password, time_shift));
+
+            // Connectivity Test
+            await media.OpenAsync();
+
+            return media;
+        }
+
+        static async Task<PTZClient> CreatePTZClientAsync(string host, string username, string password)
+        {
+            var binding = CreateBinding();
+            var device = await CreateDeviceClientAsync(new Uri($"http://{host}/onvif/device_service"), username, password);
+            var caps = await device.GetCapabilitiesAsync(new CapabilityCategory[] { CapabilityCategory.PTZ });
+            var ptz = new PTZClient(binding, new EndpointAddress(new Uri(caps.Capabilities.PTZ.XAddr)));
+
+            var time_shift = await GetDeviceTimeShift(device);
+            ptz.ChannelFactory.Endpoint.EndpointBehaviors.Clear();
+            ptz.ChannelFactory.Endpoint.EndpointBehaviors.Add(new SoapSecurityHeaderBehavior(username, password, time_shift));
+
+            // Connectivity Test
+            await ptz.OpenAsync();
+
+            return ptz;
+        }
+
+        static async Task<DeviceClient.DeviceClient> CreateDeviceClientAsync(Uri uri, string username, string password)
+        {
+            var binding = CreateBinding();
+            var endpoint = new EndpointAddress(uri);
+            var device = new DeviceClient.DeviceClient(binding, endpoint);
+            var time_shift = await GetDeviceTimeShift(device);
+
+            device = new DeviceClient.DeviceClient(binding, endpoint);
+            device.ChannelFactory.Endpoint.EndpointBehaviors.Clear();
+            device.ChannelFactory.Endpoint.EndpointBehaviors.Add(new SoapSecurityHeaderBehavior(username, password, time_shift));
+
+            // Connectivity Test
+            await device.OpenAsync();
+
+            return device;
+        }
+
+        static System.ServiceModel.Channels.Binding CreateBinding()
+        {
+            var binding = new CustomBinding();
+            var textBindingElement = new TextMessageEncodingBindingElement
+            {
+                MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.None)
+            };
+            var httpBindingElement = new HttpTransportBindingElement
+            {
+                AllowCookies = true,
+                MaxBufferSize = int.MaxValue,
+                MaxReceivedMessageSize = int.MaxValue
+            };
+
+            binding.Elements.Add(textBindingElement);
+            binding.Elements.Add(httpBindingElement);
+
+            return binding;
+        }
+
+        static async Task<TimeSpan> GetDeviceTimeShift(DeviceClient.DeviceClient device)
+        {
+            var utc = (await device.GetSystemDateAndTimeAsync()).UTCDateTime;
+            var dt = new System.DateTime(utc.Date.Year, utc.Date.Month, utc.Date.Day,
+                              utc.Time.Hour, utc.Time.Minute, utc.Time.Second);
+            return dt - System.DateTime.UtcNow;
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            if (_imaging is null) return;
+            await _imaging.MoveAsync(_token, new FocusMove
+            {
+                Relative = new RelativeFocus
+                {
+                    Distance = 0.1f,
+                    //Speed = 5f,
+                    //SpeedSpecified = true
+                }
+            });
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            if (_imaging is null) return;
+            await _imaging.MoveAsync(_token, new FocusMove
+            {
+                Relative = new RelativeFocus
+                {
+                    Distance = -0.1f,
+                    //Speed = 5f,
+                    //SpeedSpecified = true
+                }
+            });
+        }
+
+        private async void Button3_Click(object sender, EventArgs e)
+        {
+            if (_ptz is null) return;
+            await _ptz.RelativeMoveAsync(_token, new PTZVector
+            {
+                //PanTilt = new PtzService.Vector2D
+                //{
+                //    x = 0.1f,
+                //    y = 0.1f
+                //},
+                Zoom = new PtzService.Vector1D
+                {
+                    x = 1f
+                }
+            }, new PtzService.PTZSpeed
+            {
+                //PanTilt = new PtzService.Vector2D
+                //{
+                //    x = 0.1f,
+                //    y = 0.1f
+                //},
+                Zoom = new PtzService.Vector1D
+                {
+                    x = 0f
+                }
+            });
+            await Task.Delay(200);
+            await _ptz.StopAsync(_token, true, true);
+        }
+
+        private async void Button4_Click(object sender, EventArgs e)
+        {
+            if (_ptz is null) return;
+            await _ptz.RelativeMoveAsync(_token, new PTZVector
+            {
+                //PanTilt = new PtzService.Vector2D
+                //{
+                //    x = 0.1f,
+                //    y = 0.1f
+                //},
+                Zoom = new PtzService.Vector1D
+                {
+                    x = -1f
+                }
+            }, new PtzService.PTZSpeed
+            {
+                //PanTilt = new PtzService.Vector2D
+                //{
+                //    x = 0.1f,
+                //    y = 0.1f
+                //},
+                Zoom = new PtzService.Vector1D
+                {
+                    x = 0f
+                }
+            });
+            await Task.Delay(200);
+            await _ptz.StopAsync(_token, true, true);
+        }
+    }
+}
